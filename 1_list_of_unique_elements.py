ae4f5155350f4a2d024121d2f296bd20cb95750c
Marta Potocka <marta.m.slusarczyk@gmail.com>
2023-01-09 18:57:48 +0100

    Fix naming convention, update .gitignore

--- 1_list_of_unique_elements.py
+++ 1_list_of_unique_elements.py
@@ -1,13 +1,14 @@
 # QUESTION 1 - A list of unique elements
-# Using provided list A create a list B that contains only unique elements of list A.
+# Using provided original_list create a unique_list
+# that contains only unique elements of original_list.
 
-A = [1, 2, 3, 3, 2, 1, 2, 3]  # -> B = [1,2,3]
+original_list = [1, 2, 3, 3, 2, 1, 2, 3]  # -> unique_list = [1,2,3]
 
-B = []  # create empty list B
-for element in A:  # for each element in list A
-    if element not in B:  # if element does not exist in list B
-        B.append(element)  # add element to list B
-print(B)  # print list B
+unique_list = []                     # create empty list unique_list
+for element in original_list:        # for each element in list original_list
+    if element not in unique_list:   # if element does not exist in list unique_list
+        unique_list.append(element)  # add element to list unique_list
+print(unique_list)                   # print list unique_list
 
-B = list(set(A))  # create list B from the set created from list A
-print(B)
+unique = list(set(original_list))    # create unique_list from the set created from original_list
+print(unique)
