905dc14e959b71eaf7c97d7f737e9801f5ed13f8
Marta Potocka <marta.m.slusarczyk@gmail.com>
2023-01-10 22:52:09 +0100

    Add translated

--- 2_string_modification.py
+++ 2_string_modification.py
@@ -1,10 +1,14 @@
 # QUESTION 2 - What will happen when you run the code below?
 
-a = "abcdefg"            # Assign string 'abcdefg' to variable 'a'.
-print(a[1])              # Print element located under index=1 in string 'a'.
-# a[1] = 'X'             # Try to modify the string. Forbidden operation resulting in TypeError.
+my_string = "abcdefg"            # Assign string 'abcdefg' to variable 'a'.
+print(my_string[1])              # Print element located under index=1 in string 'a'.
+my_string[1] = 'X'               # Try to modify the string. This will raise TypeError
 
-a_lista = list(a)        # Create a list containing letters from string 'a'.
-a_lista[1] = 'X'         # Modify element located under index=1. List are mutable objects so this operation is allowed.
-a = "".join(a_lista)     # Create string 'a' by joining elements of a list using an empty string as a separator.
-print(a)
+string_as_list = list(my_string)        # Create a list containing letters from string 'a'.
+string_as_list[1] = 'X'                 # Modify element located under index=1. List are mutable objects so this operation is allowed.
+my_string = "".join(string_as_list)     # Create string 'a' by joining elements of a list using an empty string as a separator.
+print(my_string)
+
+new_string = my_string.replace('b', 'X')  # Create NEW string. Replace method will replace all occurence of 'b'.
+my_string = new_string                    # Assign new_string to my_string variable.
+print(my_string)
